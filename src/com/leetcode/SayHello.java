1ca291947623aa552dbc1c88b347fd3e2f90b147
Ramkumar0096 <rkum0046@gmail.com>
2026-02-09 16:06:29 +0530

    Java Learning

---
+++ src/com/leetcode/SayHello.java
@@ -0,0 +1,10 @@
+package com.leetcode;
+
+import java.util.Arrays;
+
+public class SayHello {
+    public static void main(String[] args) {
+        System.out.println("Hello Ram kumar");
+        System.out.print("vannakam developeray");
+    }
+}
