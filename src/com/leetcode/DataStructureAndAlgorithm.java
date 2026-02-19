850d2eabbdc9f264bc4d616a578ef2d627228e3b
Ramkumar0096 <rkum0046@gmail.com>
2026-02-19 22:25:27 +0530

    Find the soulution for find the peak element in the mountain

--- src/com/leetcode/DataStructureAndAlgorithm.java
+++ src/com/leetcode/DataStructureAndAlgorithm.java
@@ -92,6 +92,21 @@ class FindTheTargetFromInfinityArray{
         return -1;
     }
 }
+class FindThePeakElementInTheMountain{
+    static public int peakIndexInMountainArray(int[] arr) {
+        int start =0;
+        int end = arr.length -1;
+        while(start<end){
+            int mid = start + (end - start) / 2;
+            if(arr[mid] >arr[mid+1]){
+                end = mid;
+            }else{
+                start = mid+1;
+            }
+        }
+        return start;
+    }
+}
 
 public class DataStructureAndAlgorithm {
     static void main(String[] args)  {
--- src/com/leetcode/Leetcode.java
+++ src/com/leetcode/Leetcode.java
@@ -96,6 +96,21 @@ class FindFirstAndLastPositionInSortedArray{
         }
 
 }
+class FindThePeakElementInMountain{
+    static public int peakIndexInMountainArray(int[] arr) {
+        int start =0;
+        int end = arr.length -1;
+        while(start<end){
+            int mid = start + (end - start) / 2;
+            if(arr[mid] >arr[mid+1]){
+                end = mid;
+            }else{
+                start = mid+1;
+            }
+        }
+        return start;
+    }
+}
 
 class Leetcode {
     static void main(String[] args) {
