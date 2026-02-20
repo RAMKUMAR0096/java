9b48db7d02fa75915290bbfccbdbc0f72321a872
Ramkumar0096 <rkum0046@gmail.com>
2026-02-20 22:09:31 +0530

    Find the soultion for hard question in leetcode that is Find the target in Mountain array.The array has both asc and dsc order numbers

--- src/com/leetcode/Leetcode.java
+++ src/com/leetcode/Leetcode.java
@@ -111,6 +111,67 @@ class FindThePeakElementInMountain{
         return start;
     }
 }
+class FindTheSolutionForMountainArrayInHardQuestion1095L{
+    /**
+     * // This is MountainArray's API interface.
+     * // You should not implement it, or speculate about its implementation
+     * interface MountainArray {
+     *     public int get(int index) {}
+     *     public int length() {}
+     * }
+     */
+
+    class Solution {
+        public int findInMountainArray(int target, MountainArray mountainArr) {
+            int peak = PeakIndexInMountainArray(mountainArr);
+            int firstTry = orderAgnosticBS(mountainArr,target,0,peak);
+            if(firstTry !=-1){
+                return firstTry;
+            }
+            return orderAgnosticBS(mountainArr, target,peak+1,mountainArr.length() -1);
+        }
+        static int PeakIndexInMountainArray(MountainArray mountainArr){
+            int start =0;
+            int end = mountainArr.length() -1;
+            while(start<end){
+                int mid = start + (end -start) / 2;
+                if(mountainArr.get(mid)>mountainArr.get(mid+1)){
+                    end = mid;
+                }else{
+                    start = mid + 1;
+                }
+            }
+            return start;
+        }
+
+        static int orderAgnosticBS(MountainArray mountainArr, int target,int start,int end){
+
+            boolean isAsc = mountainArr.get(start) < mountainArr.get(end);
+            while(start<=end){
+                int mid = start + (end - start) / 2;
+
+                if(mountainArr.get(mid) == target){
+                    return mid;
+                }
+
+                if(isAsc){
+                    if(target < mountainArr.get(mid)){
+                        end = mid -1;
+                    }else{
+                        start = mid+1;
+                    }
+                }else{
+                    if(target > mountainArr.get(mid)){
+                        end = mid-1;
+                    }else{
+                        start = mid +1;
+                    }
+                }
+            }
+            return -1;
+        }
+    }
+}
 
 class Leetcode {
     static void main(String[] args) {
