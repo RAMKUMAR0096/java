0b14de97cf2f630e02ebd70e350902753f862ae6
Ramkumar0096 <rkum0046@gmail.com>
2026-02-21 22:51:00 +0530

    Find the solution for Leetcode 33 question

--- src/com/leetcode/DataStructureAndAlgorithm.java
+++ src/com/leetcode/DataStructureAndAlgorithm.java
@@ -107,12 +107,64 @@ class FindThePeakElementInTheMountain{
         return start;
     }
 }
+class FindTheTargetFromRotatedSortedArray{
+    public static int search(int[] nums, int target){
+        int pivot = findPivot(nums);
+        if(pivot == -1){
+            return binarySearch(nums,target,0,nums.length -1);
+        }
+        if(nums[pivot] == target){
+            return pivot;
+        }
+        if(target >= nums[0]){
+            return binarySearch(nums,target,0,pivot-1);
+        }
+
+        return binarySearch(nums,target,pivot+1, nums.length -1);
+
+    }
+    public static  int findPivot(int[] arr){
+        int start=0;
+        int end = arr.length -1;
+        while (start <= end){
+            int mid = start + ( end - start ) / 2;
+            if(mid < end && arr[mid] >= arr[mid+1]){
+                return mid;
+            }
+            if(mid > start &&arr[mid] <= arr[mid-1]){
+                return mid-1;
+            }
+            if(arr[mid] <= arr[start]){
+                end = mid -1;
+            }else{
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+    public static int binarySearch(int[]arr, int target, int start, int end){
+        while (start <= end){
+            int mid = start + ( end - start) / 2;
+            if(arr[mid] == target){
+                return mid;
+            }
+            if(arr[mid] > target){
+                end = mid - 1;
+            }else {
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
 
 public class DataStructureAndAlgorithm {
     static void main(String[] args)  {
-        int[] arr = {8,10,11,12,15,16,17,18,20,22,30,32,34,37,40,50,56,58,60,62,64,66,69,70,72,79,80,82,84,86,87,88,89,90};
+//        int[] arr = {8,10,11,12,15,16,17,18,20,22,30,32,34,37,40,50,56,58,60,62,64,66,69,70,72,79,80,82,84,86,87,88,89,90};
 //        System.out.println(FindTheFloorFromTheTarget.findTheFloor(arr,9));
-        System.out.println(FindTheTargetFromInfinityArray.ans(arr,41));
+//        System.out.println(FindTheTargetFromInfinityArray.ans(arr,41));
+        int arr[] = {3,5,1};
+        System.out.println(FindTheTargetFromRotatedSortedArray.search(arr,3));
 
 
     }
--- src/com/leetcode/Leetcode.java
+++ src/com/leetcode/Leetcode.java
@@ -120,6 +120,7 @@ class FindTheSolutionForMountainArrayInHardQuestion1095L{
      *     public int length() {}
      * }
      */
+    /*
 
     class Solution {
         public int findInMountainArray(int target, MountainArray mountainArr) {
@@ -171,7 +172,60 @@ class FindTheSolutionForMountainArrayInHardQuestion1095L{
             return -1;
         }
     }
+    */
 }
+class FindTheSolutionForRotatedSortedArray33L{
+    public static int search(int[] nums, int target){
+        int pivot = findPivot(nums);
+        if(pivot == -1){
+            return binarySearch(nums,target,0,nums.length -1);
+        }
+        if(nums[pivot] == target){
+            return pivot;
+        }
+        if(target >= nums[0]){
+            return binarySearch(nums,target,0,pivot-1);
+        }
+
+        return binarySearch(nums,target,pivot+1, nums.length -1);
+
+    }
+    public static  int findPivot(int[] arr){
+        int start=0;
+        int end = arr.length -1;
+        while (start <= end){
+            int mid = start + ( end - start ) / 2;
+            if(mid < end && arr[mid] >= arr[mid+1]){
+                return mid;
+            }
+            if(mid > start &&arr[mid] <= arr[mid-1]){
+                return mid-1;
+            }
+            if(arr[mid] <= arr[start]){
+                end = mid -1;
+            }else{
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+    public static int binarySearch(int[]arr, int target, int start, int end){
+        while (start <= end){
+            int mid = start + ( end - start) / 2;
+            if(arr[mid] == target){
+                return mid;
+            }
+            if(arr[mid] > target){
+                end = mid - 1;
+            }else {
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
+
+
 
 class Leetcode {
     static void main(String[] args) {
